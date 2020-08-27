88e07096c066e99b6a1ca71ba11b75c55c7c6805
codewithirene567 <irenejinscott@gmail.com>
2020-08-27 14:08:41 -0400

    changed my country object class to give the country an id and save the element to the dom

--- src/country.js
+++ src/country.js
@@ -1,6 +1,15 @@
 class Country{
+    static allArray =[]
     constructor(name) {
         this.name = name;
+        this.id = id;
+        new Country(name);
+        console.log(new Country(name))
+        this.element = document.createElement('li')
+        this.element.id = `item-${this.id}`
+        //.allCountries = document.querySelector('main')
+        //this.allCountries.appendChild(this.element)
+         Country.allArray.push(this)
       }
      
       get name() {
