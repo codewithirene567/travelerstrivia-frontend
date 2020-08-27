048758ee5a54771f60ed4b913e675e7925b4c920
codewithirene567 <irenejinscott@gmail.com>
2020-08-26 23:45:22 -0400

    made my first classes with getters and setters, also made an extended keyword for one of the countries

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 document.addEventListener('DOMContentLoaded', ()=> alert("hi"))
+
+class Country{
+    constructor(name) {
+        this._name = name;
+      }
+     
+      get name() {
+        return this.name;
+      }
+    
+      set name(name) {
+        this.name = name;
+      }
+     
+}
+
+class China extends Country {
+    constructor(name) {
+      super(name);
+      this.name = name;
+    }
+   
+  }
+
+  
