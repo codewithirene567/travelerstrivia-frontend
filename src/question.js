10d4bff8f12d7ec386cc29f823d73183c4ed912f
codewithirene567 <irenejinscott@gmail.com>
2020-08-27 11:16:40 -0400

    Changed the html to make the correct scripts appear, Added some sample header tags, Wrote a render countries function Wrote a question class object adding some event listener functions

---
+++ src/question.js
@@ -0,0 +1,14 @@
+class Question{
+    constructor(text){
+this.text = document.createElement('div')
+renderQuestions(){
+
+}
+addEventListeners(){
+    //write event listners here
+}
+this.element.addEventListeners
+renderAndAddEventListeners()
+//not sure what's supposed to go in here
+}
+}
